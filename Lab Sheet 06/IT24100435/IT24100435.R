50ddd9afe50cb19e5282e49a12158364ab45d809
Chetha Mandini <it24100435@my.sliit.lk>
2025-09-02 10:08:29 +0530

    Uploaded Lab 6

---
+++ Lab Sheet 06/IT24100435/IT24100435.R
@@ -0,0 +1,25 @@
+setwd("C:\\Users\\it24100435\\Desktop\\IT24100435")
+
+#QUESTION 1
+#1
+#Bonomial distribution because each student either 
+#passes or fails and each event is independent of one another.
+
+#2
+pbinom(47,50,0.85, lower.tail = TRUE)
+
+
+#QUESTION 2
+#1
+#Number of calls received in one hour
+
+#2
+#Poisson distribution because there is a historical value involved.
+
+#3
+dpois(15,12)
+
+
+
+
+
